56f34f3f61df8701241ffd6491fe1c8283e67136
jimmiecarlsson <jimmiecarlsson@users.noreply.github.com>
2025-09-18 09:27:37 +0000

    deploy: jimmiecarlsson/wasm.jimmiecarlsson.github.io@336a46f8343e284a365a861303f1779210ffda53

--- js/site.js
+++ js/site.js
@@ -2,3 +2,11 @@
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
     [...tooltipTriggerList].map(el => new bootstrap.Tooltip(el));
 };
+
+document.addEventListener("DOMContentLoaded", () => {
+    const hues = [30, 55, 125, 200, 280];
+    const slumpHue = hues[Math.floor(Math.random() * hues.length)];
+
+    // root
+    document.documentElement.style.setProperty("--cst-hue", slumpHue);
+});
